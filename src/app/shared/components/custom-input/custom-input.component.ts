9e8e3acbaaad1cf973811ac9a0437e8c4fbf62e3
Dam-01 <129119934+Dam-01@users.noreply.github.com>
2024-05-28 00:16:25 -0300

    Primera Version

---
+++ src/app/shared/components/custom-input/custom-input.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-custom-input',
+  templateUrl: './custom-input.component.html',
+  styleUrls: ['./custom-input.component.scss'],
+})
+export class CustomInputComponent  implements OnInit {
+
+  @Input() control: FormControl;
+  @Input() label: string;
+  @Input() iconMail: string;
+  @Input() iconPass: string;
+  @Input() type: string;
+  @Input() autocomplete: string;
+  @Input() name: string;
+  @Input() iconName: string;
+  @Input() iconConfirmPass: string;
+  
+
+  @Input() isPassword: boolean;
+  @Input() hide: boolean  = true;
+
+
+  constructor() { }
+
+  ngOnInit() {
+
+    if (this.type == 'password') this.isPassword = true;
+
+  }
+
+  showOrHidePassword(){
+    this.hide = !this.hide;
+
+    if(this.hide){
+      this.type = 'password'
+    }else{
+      this.type = 'text'
+    }
+  }
+
+}
+
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+
+    apiKey: "AIzaSyAcXns9rUy-2I_XZQcq__Nj5S1-EwWMWL0",  
+    authDomain: "taskin-project.firebaseapp.com",  
+    projectId: "taskin-project",  
+    storageBucket: "taskin-project.appspot.com",  
+    messagingSenderId: "694128015558",  
+    appId: "1:694128015558:web:596554c50090abeaeebfc7"
+      
+  }
 };
